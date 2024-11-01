155a8412c78ad8ba533dc4fdc16d645975019b1d
Development <development@Developments-MacBook-Pro.local>
2024-11-01 12:03:07 +0200

    Updating Postgres Env Prod

--- src/user/infrastructure/postgres/pg.ts
+++ src/user/infrastructure/postgres/pg.ts
@@ -1,9 +1,21 @@
 import "dotenv/config";
+import { Pool } from "pg";
 
-export const pgConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
-  port: 5432,
-};
+const pgConfig = process.env.NODE_ENV === "development"
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      user: process.env.PG_USER,
+      host: process.env.PG_HOST,
+      database: process.env.PG_DB,
+      password: process.env.PG_PASSWORD,
+      port: 5432,
+    };
+
+const pool = new Pool(pgConfig);
+
+export default pool;
