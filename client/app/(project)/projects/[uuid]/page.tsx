f9e1ea15a3ddd2c9923c97e0b16502c44bea60de
Cristian Machuca <cmachuca32@gmail.com>
2024-11-30 01:29:34 +0200

    Updated UI Card by ID

--- client/app/(project)/projects/[uuid]/page.tsx
+++ client/app/(project)/projects/[uuid]/page.tsx
@@ -30,15 +30,14 @@ async function getProjectById(uuid: string): Promise<TProject | null> {
   return project;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default async function ProjectDetails({ params }: any) {
+export default async function ProjectDetails({ params }: { params: { uuid: string } }) {
   const project = await getProjectById(params.uuid);
   const userProjects = await getAllUserProject();
 
   if (!project) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <h2 className="text-center my-5 text-4xl md:text-5xl lg:text-6xl font-extrabold bg-gradient-to-r from-orange-400 to-yellow-500 bg-clip-text text-transparent">
+      <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-orange-100 to-yellow-100">
+        <h2 className="text-center text-5xl md:text-6xl lg:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-yellow-500">
           Project Not Found
         </h2>
       </div>
@@ -46,56 +45,59 @@ export default async function ProjectDetails({ params }: any) {
   }
 
   return (
-    <div className="max-w-3xl mx-auto p-4 bg-white rounded-md  font-sans text-gray-400 overflow-y-auto mt-5 sm:mb-0 shadow-2xl">
-      <h2 className="text-center my-7 text-4xl md:text-5xl lg:text-6xl font-extrabold bg-gradient-to-r from-orange-400 to-yellow-500 bg-clip-text text-transparent">
+    <div className="max-w-3xl mx-auto p-8 bg-white rounded-2xl shadow-xl font-sans text-gray-400 overflow-hidden mt-10 sm:mt-12 sm:mb-0">
+      <h2 className="text-center my-5 text-5xl md:text-6xl lg:text-7xl font-extrabold bg-gradient-to-r from-orange-400 to-yellow-500 bg-clip-text text-transparent">
         {project.title}
       </h2>
-      <div className="mb-6 flex justify-center bg-gradient-to-r from-orange-400 to-yellow-500 p-8 shadow-2xl rounded-md">
+      
+      <div className="mb-8 flex justify-center bg-gradient-to-r from-orange-400 to-yellow-500 p-12 shadow-2xl rounded-2xl">
         {project.image ? (
           <Image
             src={project.image}
             alt={project.title}
-            width={600}
-            height={400}
-            className="w-60 h-60 object-cover rounded-lg my-4"
+            width={750}
+            height={450}
+            className="w-full h-72 object-cover rounded-lg shadow-md"
           />
         ) : (
-          <div className="w-96 h-64 flex items-center justify-center bg-gradient-to-r from-orange-400 to-yellow-500 bg-clip-text text-transparent rounded-lg">
-            <span className="text-gray-500 text-center">
-              No image available
-            </span>
+          <div className="w-full h-72 flex items-center justify-center bg-gradient-to-r from-orange-400 to-yellow-500 text-gray-500 rounded-lg shadow-md">
+            <span>No image available</span>
           </div>
         )}
       </div>
-      <div className="flex justify-end w-full">
+
+      <div className="flex justify-end w-full mb-6">
         <Link
           href="/"
-          className="text-white px-6 py-2 mb-2 bg-gradient-to-r from-yellow-400 to-orange-500 hover:from-orange-500 hover:to-yellow-400 transition-colors rounded-lg font-medium shadow-lg shadow-orange-300"
+          className="text-white px-8 py-3 bg-gradient-to-r from-yellow-400 to-orange-500 hover:from-orange-500 hover:to-yellow-400 transition-all ease-in-out duration-300 rounded-lg font-semibold shadow-lg shadow-orange-300"
         >
           Back
         </Link>
       </div>
-      <p className="text-lg mb-4">{project.description}</p>
-      <h2 className="text-orange-300 text-lg font-semibold mb-2">
-        Technologies:
-      </h2>
-      <ul className="list-disc pl-6">
-        {project.technologies.map((tech) => (
-          <li key={tech} className="text-md">
-            {tech}
-          </li>
-        ))}
-      </ul>
-      <p className="text-lg text-orange-300 font-semibold">Team applied:</p>
-      <ul className="list-none pl-6 mb-12">
-        {userProjects
-          ?.filter((userProject) => userProject.project_id === project.id)
-          .map((applicant) => (
-            <li key={applicant.user_email} className="text-md list-disc">
-              {applicant.user_name}
-            </li>
+
+      <p className="text-lg mb-6">{project.description}</p>
+
+      <div>
+        <h3 className="text-orange-300 text-xl font-semibold mb-3">Technologies:</h3>
+        <ul className="list-disc pl-6 space-y-2">
+          {project.technologies.map((tech) => (
+            <li key={tech} className="text-md">{tech}</li>
           ))}
-      </ul>
+        </ul>
+      </div>
+
+      <div className="mt-8">
+        <p className="text-xl text-orange-300 font-semibold mb-3">Team applied:</p>
+        <ul className="list-none pl-6 mb-12 space-y-2">
+          {userProjects
+            ?.filter((userProject) => userProject.project_id === project.id)
+            .map((applicant) => (
+              <li key={applicant.user_email} className="text-md list-disc">
+                {applicant.user_name}
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
