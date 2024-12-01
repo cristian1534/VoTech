b95b25664e0ee96b07935fb45f71e1cb63008359
Cristian Machuca <cmachuca32@gmail.com>
2024-12-02 01:20:50 +0200

    Udpated app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ import swaggerUI from "swagger-ui-express";
 import { options } from "./user/infrastructure/documentation/swagger.documentation";
 import { connectRedis } from "./user/infrastructure/redis/redis";
 import morgan from "morgan";
-import axios from "axios";
 
 const app = express();
 (async () => {
@@ -20,7 +19,6 @@ const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.static(path.join(__dirname, "user/infrastructure/postgres")));
-app.use(express.raw({ type: 'application/x-www-form-urlencoded' })); // Para manejar datos en formato x-www-form-urlencoded
 
 const specs = swaggerJSDoc(options);
 
@@ -30,27 +28,6 @@ app.use("/projects", projectRoutes);
 app.use("/user-project", userProjectRoutes);
 app.use("/contacts", contactRoutes);
 
-app.post("/api/paypal/ipn", async (req, res) => {
-  const ipnData = req.body.toString(); 
-
-  const validationParams = new URLSearchParams(ipnData);
-  validationParams.append('cmd', '_notify-validate');
-
-  try {
-    const response = await axios.post('https://ipnpb.sandbox.paypal.com/cgi-bin/webscr', validationParams);
-
-    if (response.data === 'VERIFIED') {
-      console.log('IPN válida:', ipnData);
-      res.status(200).send('IPN procesada');
-    } else {
-      console.log('IPN no verificada');
-      res.status(400).send('IPN no verificada');
-    }
-  } catch (error) {
-    console.error('Error al validar la IPN', error);
-    res.status(500).send('Error al validar la IPN');
-  }
-});
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
