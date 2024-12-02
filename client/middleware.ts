eb4dc63fca524e2e605f3892a19e38dc985e40e0
Cristian Machuca <cmachuca32@gmail.com>
2024-12-02 12:23:25 +0200

    Test Deployment Payment Controller

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const email = req.cookies.get("email")?.toString();
-  const token = req.cookies.get("token")?.toString();
-  const paymentCompleted = req.cookies.get("paymentCompleted");
+  const email = req.cookies.get("email")?.value;
+  const token = req.cookies.get("token")?.value;
+  const paymentCompleted = req.cookies.get("paymentCompleted")?.value;
   const url = req.nextUrl.clone();
 
   if (url.pathname.startsWith("/dashboard")) {
-    if (email !== "admin@votech.com" || !token) {
+    if (!email || !token || email !== "admin@votech.com") {
       url.pathname = "/";
       return NextResponse.redirect(url);
     }
@@ -21,6 +21,8 @@ export function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
 export const config = {
   matcher: ["/dashboard/:path*", "/signup"],
 };
+
