7f1bd0986ab09b8f009144812d2df14990b456f6
Cristian Machuca <cmachuca32@gmail.com>
2024-12-14 21:11:22 +0200

    Updates to 2 Inst

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -4,7 +4,7 @@ module.exports = {
         name: 'app',
         script: './src/app.ts',
         interpreter: 'ts-node', 
-        instances: 1, 
+        instances: 2, 
         exec_mode: 'cluster', 
         env: {
           NODE_ENV: 'production', 
