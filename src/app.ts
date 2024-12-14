61fed754de43171db5dc44d55e960cca7b6528ff
Cristian Machuca <cmachuca32@gmail.com>
2024-12-14 19:46:02 +0200

    Updates to 2 Inst

--- src/app.ts
+++ src/app.ts
@@ -42,10 +42,10 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
   process.env.NODE_ENV === "development"
     ? console.log(`Server running at ${PORT} Development`)
