56bfc9de179adcd7078dc39d7460bd93b3d19e00
Cristian Machuca <cmachuca32@gmail.com>
2024-12-17 03:31:41 +0200

    Chat Room created.

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,51 @@ import { options } from "./user/infrastructure/documentation/swagger.documentati
 import { connectRedis } from "./user/infrastructure/redis/redis";
 import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
 import morgan from "morgan";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"], 
+  },
+});
+
+
+let onlineUsers: string[] = [];
+
+io.on('connection', (socket) => {
+  let nickname = '';
+
+  socket.on('setNickname', (name) => {
+    nickname = name;
+    onlineUsers.push(nickname); 
+    io.emit('userConnected', nickname);  
+    io.emit('userCount', onlineUsers.length);  
+  });
+
+  socket.on('typing', (user) => {
+    socket.broadcast.emit('userTyping', user);
+  });
+
+  socket.on('stopTyping', () => {
+    socket.broadcast.emit('userStopTyping');
+  });
+
+  socket.on('sendMessage', ({ sender, message }) => {
+    io.emit('receiveMessage', { sender, message });
+  });
+
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter(user => user !== nickname);  
+    io.emit('userDisconnected', nickname);  
+    io.emit('userCount', onlineUsers.length); 
+  });
+});
+
 const swaggerUiOptions = {
   explorer: true,
   customCss: new SwaggerTheme().getBuffer(SwaggerThemeNameEnum.ONE_DARK),
@@ -27,10 +70,14 @@ const swaggerUiOptions = {
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "user/infrastructure/postgres")));
+app.use(express.static(path.join(__dirname, "user/infrastructure/postgres")));  
 
 const specs = swaggerJSDoc(options);
-app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(specs, swaggerUiOptions));
+app.use(
+  "/api/v1/docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, swaggerUiOptions)
+);
 
 app.use("/users", userRoutes);
 app.use("/projects", projectRoutes);
@@ -39,16 +86,13 @@ app.use("/contacts", contactRoutes);
 app.use("/portfolio", portfolioRoutes);
 app.use("/newsletter", newsLetterRoutes);
 
-
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = Number(process.env.PORT) || 4000;
 
-
-app.listen(PORT,"0.0.0.0", () => {
-
+server.listen(PORT, "0.0.0.0", () => {
   process.env.NODE_ENV === "development"
     ? console.log(`Server running at ${PORT} Development`)
     : console.log(`Server running at ${PORT} Production`);
