851fec8f7a9cbd84b3f66dd100f5fee5f9b1ad59
Cristian Machuca <cmachuca32@gmail.com>
2025-01-08 13:17:02 +0200

    Updated Job Card Delete Icon

--- client/components/JobCard.tsx
+++ client/components/JobCard.tsx
@@ -4,15 +4,20 @@ import React from "react";
 import { Job } from "../types/typeJobs";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import { deleteJobByUuid } from "../lib/api";
+import { useSession } from "../context/SessionContext";
 
 export const JobCard = ({ uuid, title, description, contact }: Job) => {
+  const { sessionEmail } = useSession();
+
   return (
     <div className="max-w-md p-4 border border-gray-200 rounded-md shadow-lg bg-white hover:shadow-2xl transition-shadow flex flex-col">
       <div className="flex justify-between items-center">
         <h3 className="text-xl font-bold text-orange-400">{title}</h3>
-        <div className="text-red-500 cursor-pointer">
-          <RiDeleteBin5Line size={20} onClick={() =>deleteJobByUuid(uuid)}/>
-        </div>
+        {sessionEmail === "admin@votech.com" && (
+          <div className="text-red-500 cursor-pointer">
+            <RiDeleteBin5Line size={20} onClick={() => deleteJobByUuid(uuid)} />
+          </div>
+        )}
       </div>
 
       <p className="mt-2 text-gray-400 flex-grow">{description}</p>
