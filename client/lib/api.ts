b2c53c01837f4defc9cce3f534c2f2a0b790759a
Cristian Machuca <cmachuca32@gmail.com>
2025-01-10 22:31:40 +0200

    Updated Deployment

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -53,8 +53,14 @@ export async function getUsers(): Promise<TUser[] | null> {
   try {
     const response = await axios.get<UserApiResponse>(
       "https://votech.onrender.com/users/",
+      {
+        headers: {
+          "Cache-Control": "no-store",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
     );
-    setTimeout(() => { window.location.reload()}, 1000);
     return response.data.data || null;
   } catch (error) {
     console.error("Axios error:", error);
